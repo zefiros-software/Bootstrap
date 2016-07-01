ee5c49c4a89cc395b3957d1bbbd552333fc944a3
PaulVisscher <paul.pev@hotmail.com>
2016-07-02 00:02:05 +0200

    canonicalize

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for filename in bin/premake*; do    
-    file = $(readlink -f "./$filename")
+    file = $(readlink --canonicalize "./$filename")
     echo "$file test --file=test/tests.lua --systemscript=- --scripts=../"
     "$file test --file=test/tests.lua --systemscript=- --scripts=../"
 done
