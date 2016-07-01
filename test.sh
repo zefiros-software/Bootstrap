d4af946f0e50c6bbb5cd642011cc2b2c1440f111
PaulVisscher <paul.pev@hotmail.com>
2016-07-01 23:53:21 +0200

    Syntax fix

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for filename in bin/premake*; do    
-    echo (readlink -f "./"$filename) " test --file=test/tests.lua --systemscript=- --scripts=../"
-    (readlink -f "./"$filename) " test --file=test/tests.lua --systemscript=- --scripts=../"
+    $file = (readlink -f "./$filename")
+    echo "file test --file=test/tests.lua --systemscript=- --scripts=../"
+    "file test --file=test/tests.lua --systemscript=- --scripts=../"
 done
