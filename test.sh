8640dc04563300f2e99a2a16c5525fbc1aabac3a
PaulVisscher <paul.pev@hotmail.com>
2016-07-01 23:39:55 +0200

    Path fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for filename in /bin/premake*; do    
+for filename in bin/premake*; do    
     echo $filename "test --file=test/tests.lua --systemscript=- --scripts=../"
     $filename "test --file=test/tests.lua --systemscript=- --scripts=../"
 done
