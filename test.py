bd6f0eaeccbd150625cd7c62a0be9aee32b1a5e6
PaulVisscher <paul.pev@hotmail.com>
2016-07-02 00:38:26 +0200

    Exit test runner

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import os, glob
 for file in glob.glob( "bin/premake*" ):
     command = "%s test --file=test/tests.lua --systemscript=- --scripts=./" % file
     print( command )
-    os.system( command )
+    code = os.system( command )
+    if code != 0:
+        exit(-1)
