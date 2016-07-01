8280668e0b169acad5c62ec6dc9ee6b574681b41
PaulVisscher <paul.pev@hotmail.com>
2016-07-02 00:17:30 +0200

    Python test runner

--- test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /r "bin" %%a in (*.exe) do (
-    echo "%%~fa" test --file=test/tests.lua --systemscript=- --scripts=../
-    "%%~fa" test --file=test/tests.lua --systemscript=- --scripts=../
-)
---
+++ test.py
@@ -0,0 +1,6 @@
+import os, glob
+
+for file in glob.glob( "bin/premake*" ):
+    command = "%s test --file=test/tests.lua --systemscript=- --scripts=../" % file
+    print( command )
+    os.system( command )
