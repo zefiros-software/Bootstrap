5cbe58bc96d7f8d426589e6f141574d352912321
PaulVisscher <paul.pev@hotmail.com>
2016-07-01 23:44:04 +0200

    Test runner fix

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 for filename in bin/premake*; do    
     echo $filename "test --file=test/tests.lua --systemscript=- --scripts=../"
-    $filename "test --file=test/tests.lua --systemscript=- --scripts=../"
+    "./"$filename " test --file=test/tests.lua --systemscript=- --scripts=../"
 done
